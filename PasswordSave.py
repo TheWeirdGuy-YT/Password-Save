eb2166b4fa8c1b419e2d128c65601a5e99e04ff6
TheWeirdGuy-YT <61080865+TheWeirdGuy-YT@users.noreply.github.com>
2020-02-15 13:20:05 +0100

    Added the Script File you need to run this

---
+++ PasswordSave.py
@@ -0,0 +1,17 @@
+#Made by TheWeirdGuy-YT
+
+print ("Hello, how are you doing?")
+print ("Please enter your Password into the Textbox")
+print ("This is still in development... I might introduce Multi Capability in the Future so that everything has a Different Password")
+password_info = input("Password")
+
+if password_info == "Password":
+    print ("Wifi Password:  Password here")
+    print ("Credit Card:    Password here")
+    print ("Email Address:  ???@???.???")
+    print ("Email Password: Password here")
+else:
+    print ("Nope, thats not the Password")
+    input ("Press Enter to exit")
+
+input("Press Enter to exit")
